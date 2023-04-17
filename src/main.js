fe614b2603d0245e01d4e84463ffd819c75244aa
luca-macedone <luca.macedone@outlook.it>
2023-04-17 15:22:29 +0200

    initial commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue';
+import '/node_modules/bootstrap/dist/js/bootstrap.js'
+import './assets/sass/app.scss'
+import App from './App.vue';
+
+createApp(App).mount('#app');
